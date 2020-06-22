146f8abbdaa53a76fd953808120cb214314da648
vadim2853 <40173027+vadim2853@users.noreply.github.com>
2020-06-22 13:14:21 +0300

    Update lnamp.sh

--- lnamp.sh
+++ lnamp.sh
@@ -2,10 +2,14 @@
 # server LNMAP
 apt-get update && apt-get upgrade -y
 
+# paste site
+rm -r /var/www/html
+cp ./html /var/www/html
+
 #install php-fpm
 apt-get install -y php php-fpm
-systemctl enable php7.3-fpm
-systemctl start php7.3-fpm
+systemctl enable php7.2-fpm
+systemctl start php7.2-fpm
 
 # install nginx
 apt-get install -y nginx
@@ -14,3 +18,5 @@ rm -r cd /etc/nginx/sites-available /etc/nginx/sites-enabled /etc/nginx/nginx.co
 # paste .conf in nginx
 cp ./conf/nginx/conf.d/wordpress.conf /etc/nginx/conf.d/wordpress.conf
 cp ./conf/nginx/nginx.conf /etc/nginx/nginx.conf
+# restart && enabled nginx
+systemctl restart nginx
