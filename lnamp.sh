7fab29d50124b75c54b0cbdbf3f5901b69c05334
Viktoria <viktoria@lits-admins-MacBook-Pro-2.local>
2020-07-09 21:41:56 +0300

    LNAMP deployment script

--- lnamp.sh
+++ lnamp.sh
@@ -1,23 +1,106 @@
 #!/bin/sh
-# server LNMAP
-apt-get update && apt-get upgrade -y
+printf "
+#######################################################################
+#                     LAMP deployment script                          #
+#######################################################################
+"
+
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Read user info for db
+read -p 'Enter db name: ' db_name
+
+read -p 'Enter db user: ' db_user
+
+read -sp 'Enter db user password: ' db_user_password
+
+echo Summary:
+echo db name: $db_name
+echo db user: $db_user
+
+export DB_NAME=$db_name
+export DB_USER=$db_user
+export DB_USER_PASSWORD=$db_user_password
+
+
+# Update packages
+apt-get update
+read -p 'Do you want to upgrade packages? Type "y" if yes: ' need_upgrade
+if [ $need_upgrade == 'y' ]
+then
+  apt-get upgrade -y
+else
+  echo System will not be upgraded
+fi
 
 # install nginx
 apt-get install -y nginx
+
 # delete default .conf folder nginx
-rm -r cd /etc/nginx/sites-available /etc/nginx/sites-enabled /etc/nginx/nginx.conf
+rm -r /etc/nginx/sites-available /etc/nginx/sites-enabled
+
 # paste .conf in nginx
-cp -r ./conf/nginx/conf.d/ /etc/nginx/conf.d/
+cp -r ./conf/nginx/conf.d/ /etc/nginx/
 cp ./conf/nginx/nginx.conf /etc/nginx/nginx.conf
 
-#install php-fpm
-apt-get install -y php php-fpm
-systemctl enable php7.2-fpm
-systemctl start php7.2-fpm
 
-# restart && enabled nginx
-systemctl restart nginx
+# install php
+apt install php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip -y
+
+# enable php mod
+a2enmod php7.4
+
+# enable and start php
+systemctl enable php7.4-fpm
+systemctl start php7.4-fpm
+
+# install apache
+apt-get install -y apache2 libapache2-mod-php
+cp ./conf/apache/ports.conf /etc/apache2/ports.conf
+cp ./conf/apache/apache2.conf /etc/apache2/apache2.conf
+cp ./conf/apache/dir.conf /etc/apache2/mods-available/dir.conf
+
+
+# install MySQL
+apt install mysql-server
+
+echo You can create new db for wordpress or import db from sql file.
+echo To import db from file put your file to conf/mysql dir with filename backup.sql
+read -p 'Do you want to create new db or import from sql? Type "n" for new or "i" for import: ' db_create_option
+if [ $db_create_option == 'n' ]
+then
+  mysql -u root < conf/mysql/base_install.sql
+elif [ $db_create_option == 'i' ]
+then
+  FILE=/etc/resolv.conf
+  if [ -f 'conf/mysql/backup.sql' ]
+  then
+    mysql -u root < conf/mysql/backup.sql
+  else
+    echo There no file backup.sql in conf/mysql/ directory!!!
+    exit 1
+  fi
+else
+  echo Wrong option!!!
+  exit 1
+fi
+
+
+
 
 # paste site
-rm -r /var/www/html
-cp -r ./html /var/www/html
+rm -r /var/www/wordpress
+cp -r ./wordpress /var/www/wordpress
+sudo chown -R www-data:www-data /var/www/wordpress/
+sudo chmod -R 755 /var/www/wordpress/
+
+# enable and start nginx
+systemctl enable nginx
+systemctl start nginx
+
+# enable and start apache
+sudo systemctl enable apache2
+sudo systemctl start apache2
